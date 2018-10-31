5124b277f3e5ef9a638284a5717a20cc6745298e
jtigchelaar <jtigchelaar@voogd.com>
2018-10-31 11:34:33 +0100

    Change target name

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-gcc -I/opt/vc/include -I/opt/vc/include/interface/vmcs_host/linux -I/opt/vc/include/interface/vcos/pthreads main.c -o db_splash -lshapes
+gcc -I/opt/vc/include -I/opt/vc/include/interface/vmcs_host/linux -I/opt/vc/include/interface/vcos/pthreads main.c -o splash -lshapes
